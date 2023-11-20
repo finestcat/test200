33c17f82943a72fb99f5149945859ea045e38d9c
finestcat <125952040+finestcat@users.noreply.github.com>
2023-11-20 13:30:53 +0800

    Revert "26"
    This reverts commit 6954fb784903bfe52d934d67be40fae27c3b4fbc.

--- server.js
+++ server.js
@@ -1,115 +1,84 @@
-//Define constant for the application
-const express = require("express");
-const app = express();
-const assert = require('assert');
+const express = require('express');
 const session = require('cookie-session');
-const bodyParser = require('body-parser')
-const MongoClient =require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
+const bodyParser = require('body-parser');
+const app = express();
 const ejs = require('ejs');
 const fs = require('fs');
 const formidable = require('express-formidable');
-const mongourl = 'mongodb+srv://admin:123@cluster0.nndk5cp.mongodb.net/?retryWrites=true&w=majority';
+const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const mongourl = 'mongodb+srv://admin:123@cluster0.nndk5cp.mongodb.net/?retryWrites=true&w=majority'; 
 const dbName = 'Project';
 
-app.set('view engine', 'ejs');
+app.set('view engine','ejs');
 app.set('views','./views');
 
-//Authenication
-const SECRETKEY = '381F-Project';
+const SECRETKEY = 'COMPS381F';
 
 const users = new Array(
-	{name: 'Issac', password: 'Issac'},
-	{name: 'Sam', password:'Sam'},
-	{name: 'guest', password: 'guest'}
+	{name: 'user1', password: 'user1'},
+	{name: 'user2', password: 'user2'}
 );
-
 var document = {}
-
 
 app.use(session({
   name: 'loginSession',
   keys: [SECRETKEY]
 }));
 
+// support parsing of application/json type post data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// View Part
-
-app.get('/',(req,res) => {
-  if (!req.session.authenticated) {    
-		res.redirect('/login');
-  }
-  else{
-    res.redirect('/main');
-  }
-});
-
-// Redirect to Login Page (Not Logging in)/ Main Page (Logging in)
-app.get('/main', async (req,res) => {
+app.get('/', (req,res) => {
 	console.log(req.session);
-    if (!req.session.authenticated) {    
+	if (!req.session.authenticated) {    // user not logged in!
 		res.redirect('/login');
 	} else {
-        const client = new MongoClient(mongourl);
-        await client.connect();
-        const db = client.db(dbName);
-        var dataSet = new Array();
-        //res.render('main',{name:req.session.username});
-        //read data
-        const data = db.collection("Inventory").find().sort({"quantity" : 1});
-        await data.forEach((element) =>{
-            dataSet.push(element);
-        });
-
-        //Check the whole array
-        //console.log({dataSet:dataSet});
-        res.status(200).render('main',{dataSet,name:req.session.username});
-        //res.status(200).render('main',{name:req.session.username},);
-        client.close();
+		res.status(200).render('home',{name:req.session.username});
 	}
 });
 
-
-// Load Login page
 app.get('/login', (req,res) => {
 	res.status(200).render('login',{});
 });
 
-//login function
 app.post('/login', (req,res) => {
-	var username = req.body.username;
-	var password = req.body.password;
 	users.forEach((user) => {
-    console.log(user.username)
-		if (user.name == username && user.password == password) {
+		if (user.name == req.body.name && user.password == req.body.password) {
+			// correct user name + password
+			// store the following name/value pairs in cookie session
 			req.session.authenticated = true;        // 'authenticated': true
-			req.session.username = req.body.username;
+			req.session.username = req.body.name;	 // 'username': req.body.name		
 		}
 	});
-	res.redirect('/main');
+	res.redirect('/');
 });
 
-// Load Main Page
-app.get('/main',(req, res) => {
-	console.log("Enter Main Page Successfully")
+app.get('/details', (req,res) => {
+
+        const client = new MongoClient(mongourl);
+        client.connect();
+        const db = client.db(dbName);
+        var dataSet = new Array();
+
+        const data = db.collection("Inventory").find().sort({"quantity" : 1});
+        data.forEach((element) =>{
+            dataSet.push(element);
+        });
+
+        res.status(200).render('details',{dataSet});
+
+        client.close();
+	
 });
 
-// Logout function
-app.get('/logout', (req,res) => {
-	req.session = null;   // clear cookie
-	res.redirect('/login');
+
+app.get('/create', (req,res) => {
+	res.status(200).render('create');
 });
 
-// Render to Create Page
-app.get('/create',(req, res) =>{
-        res.status(200).render("create");
-
-}); 
-
-// Create 
-// Create Function
 const createDocument = (db, createDoc, callback) => {
     db.collection('Inventory').insertOne(createDoc, (error, results) => {
         if (error) throw error;
@@ -117,19 +86,19 @@ const createDocument = (db, createDoc, callback) => {
         callback();
     });
 };
-
-app.post('/create', (req, res) => {
-    console.log("User entered create page");
+app.post('/create', (req,res) => {
     const client = new MongoClient(mongourl);
     client.connect((err) => {
         assert.equal(null, err);
         console.log("Connected successfully to MongoDB.");
         const db = client.db(dbName);
         const document = {	
-            inv_id: req.body.id,
-            inv_name: req.body.inv_name,
-            inv_type: req.body.type,
-            quantity: parseInt(req.body.quantity)
+            id: req.body.id,
+            name: req.body.name,
+            category: req.body.category,
+            status: req.body.status,
+			location: req.body.location,
+			date: req.body.date
         };
 
         // Check all the fields of the form are filled in
@@ -138,14 +107,12 @@ app.post('/create', (req, res) => {
             console.log("Created new document successfully");
             client.close();
             console.log("Closed DB connection");
-            res.redirect('/main');
+            res.redirect('/details');
     });
     client.close();
 });
-
 });
 
-//Delete
 app.get('/delete?:id', (req,res) => {
     console.log("User entered delete page");
     const client = new MongoClient(mongourl);
@@ -162,80 +129,71 @@ app.get('/delete?:id', (req,res) => {
             console.log("Data has been deleted");
             });
         });
-        res.redirect('/main');
-    });
-
-//Delete
-app.get('/delete?:id', (req,res) => {
-  console.log("User entered delete page");
-  //Create new client to fetch the data
-  const client = new MongoClient(mongourl);
-  //Catch the specific id and used as a conditional parameter
-  const deletedID = req.query._id;
-  client.connect((err) => {
-      assert.equal(null, err);
-      const db = client.db(dbName);
-      db.collection("Inventory").deleteOne({_id: ObjectID(deletedID)},(err,result) =>{
-          if(err)
-          throw err;
-          client.close();
-          console.log("Data has been deleted");
-          });
-      });
-      res.redirect('/main');
-  });
-
-
-// Update 
-app.get('/update', async (req, res) => {
-  const client = new MongoClient(mongourl);
-  const id = req.query._id; // Get the _id from the query parameter
-  console.log("_id:",id);
-  // Connect to MongoDB
-  await client.connect();
-  const db = client.db(dbName);    
-  // Fetch the document to be updated
-  const item = await db.collection("Inventory").findOne({ _id: ObjectID(id) });
-
-  // Render the update.ejs template with the item data
-  res.render('update', { item });
+        res.redirect('/details');
 });
 
-app.post('/update', (req, res) => {
-  const client = new MongoClient(mongourl);
-  const id = req.body._id; // Get the id from the request body
-  console.log("_id:",id);
-
-  // Connect to MongoDB
-  client.connect((err) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to MongoDB.");
-    const db = client.db(dbName);
-
-    // Construct the update query
-    const updateDoc = {
-      $set: { // Auto fill in the form with existing info
-      	inv_id: req.body.id,
-        inv_name: req.body.inv_name,
-        inv_type: req.body.type,
-        quantity: parseInt(req.body.quantity)
-      }
-    };
-
-    // Perform the update operation
-    db.collection('Inventory').updateOne({ _id: ObjectID(id) }, updateDoc, (error, result) => {
-      if (error) throw error;
-      console.log('Document updated successfully');
-      client.close();
-      console.log("Closed DB connection");
-      res.redirect('/main');
-    });
-  });
+app.get('/edit', (req,res) => {
+	// res.status(200).render('edit',{});
+	const client = new MongoClient(mongourl);
+	const id = req.query._id; // Get the _id from the query parameter
+	console.log("_id:",id);
+	// Connect to MongoDB
+	client.connect();
+	const db = client.db(dbName);    
+	// Fetch the document to be updated
+	const item = db.collection("Inventory").findOne({ _id: ObjectID(id) });
+  
+	// Render the update.ejs template with the item data
+	res.render('update', { item });
 });
 
-// Restful API
+app.post('/edit', (req,res) => {
+	const client = new MongoClient(mongourl);
+	const id = req.body._id; // Get the id from the request body
+	console.log("_id:",id);
+  
+	// Connect to MongoDB
+	client.connect((err) => {
+	  assert.equal(null, err);
+	  console.log("Connected successfully to MongoDB.");
+	  const db = client.db(dbName);
+  
+	  // Construct the update query
+	  const updateDoc = {
+		$set: { // Auto fill in the form with existing info
+			id: req.body.id,
+            name: req.body.name,
+            category: req.body.category,
+            status: req.body.status,
+			location: req.body.location,
+			date: req.body.date
+		}
+	  };
+  
+	  // Perform the update operation
+	  db.collection('Inventory').updateOne({ _id: ObjectID(id) }, updateDoc, (error, result) => {
+		if (error) throw error;
+		console.log('Document updated successfully');
+		client.close();
+		console.log("Closed DB connection");
+		res.redirect('/details');
+	  });
+	});
+});
 
-// Create API
+app.get('/search', (req,res) => {
+	res.status(200).render('search',{});
+});
+
+app.post('/search', (req,res) => {
+//
+});
+
+app.get('/logout', (req,res) => {
+	req.session = null;   // clear cookie-session
+	res.redirect('/');
+});
+
 app.post('/api/inventory', (req,res) => {
     console.log("Create with API");
     const client = new MongoClient(mongourl);
@@ -244,10 +202,12 @@ app.post('/api/inventory', (req,res) => {
         console.log("Connected successfully to MongoDB.");
         const db = client.db(dbName);
         const document = {	
-            inv_id: req.body.inv_id,
-            inv_name: req.body.inv_name,
-            inv_type: req.body.inv_type,
-            quantity: parseInt(req.body.quantity)
+            id: req.body.id,
+            name: req.body.name,
+            category: req.body.category,
+            status: req.body.status,
+			location: req.body.location,
+			date: req.body.date
         };
 
         // Check all the fields of the form are filled in
@@ -259,87 +219,84 @@ app.post('/api/inventory', (req,res) => {
     });
     client.close();
 });
+});
 
-
+app.get('/api/inventory/:id', (req, res) => {
+	console.log("Read with API");
+	  if (req.params.inv_id) {
+			  let inventory = {};
+			  inventory['id'] = req.params.id;
+			  const client = new MongoClient(mongourl);
+			  client.connect((err) => {
+					assert.equal(null, err);
+					const db = client.db(dbName);
+					let cursor = db.collection('Inventory').find(inventory);
+					console.log(`findDocument: ${JSON.stringify(inventory)}`);
+					cursor.toArray((err, docs) => {
+				  assert.equal(err, null);
+				  console.log(`findDocument: ${docs.length}`);
+				  client.close();
+				  console.log("Closed DB connection");
+				  res.status(200).json(docs);
+				});
+		  });
+	} else {
+	  res.status(500).json({ "error": "missing inv_id" }).end();
+	}
 });
 
 
-// Read API
-app.get('/api/inventory/:inv_id', (req, res) => {
-  console.log("Read with API");
-	if (req.params.inv_id) {
-    		let inventory = {};
-    		inventory['inv_id'] = req.params.inv_id;
-    		const client = new MongoClient(mongourl);
-    		client.connect((err) => {
-      			assert.equal(null, err);
-      			const db = client.db(dbName);
-      			let cursor = db.collection('Inventory').find(inventory);
-      			console.log(`findDocument: ${JSON.stringify(inventory)}`);
-      			cursor.toArray((err, docs) => {
-        		assert.equal(err, null);
-        		console.log(`findDocument: ${docs.length}`);
-        		client.close();
-        		console.log("Closed DB connection");
-        		res.status(200).json(docs);
-      		});
-    	});
-  } else {
-    res.status(500).json({ "error": "missing inv_id" }).end();
+app.put('/api/inventory/:_id', async (req,res) => {
+	console.log("Update with API");
+	const client = new MongoClient(mongourl);
+	await client.connect();
+	const db = client.db(dbName);
+	const updateDoc = {
+	  $set: {
+		id: req.body.id,
+		name: req.body.name,
+		category: req.body.category,
+		status: req.body.status,
+		location: req.body.location,
+		date: req.body.date
+	  }
+	};
+	const data = await db.collection('Inventory').findOne({id: req.params.id});
+	if(data){
+	await db.collection('Inventory').updateOne({_id: data._id}, updateDoc, (error, result) => {
+	if (error) throw error;
+	console.log('Document updated successfully from inv_id ' + req.params.id + ' to ' + req.body.id);
+	client.close();
+	console.log("Closed DB connection");
+  });
+  }else{
+	res.status(404).json({ message: 'Invalid inventory_id' });
   }
+	client.close();
 });
 
-// Update API
-app.put('/api/inventory/:inv_id', async (req,res) => {
-  console.log("Update with API");
-  const client = new MongoClient(mongourl);
-  await client.connect();
-  const db = client.db(dbName);
-  const updateDoc = {
-    $set: {
-      inv_id: req.body.inv_id,
-      inv_name: req.body.inv_name,
-      inv_type: req.body.inv_type,
-      quantity: parseInt(req.body.quantity)
-    }
-  };
-  const data = await db.collection('Inventory').findOne({inv_id: req.params.inv_id});
-  if(data){
-  await db.collection('Inventory').updateOne({_id: data._id}, updateDoc, (error, result) => {
-  if (error) throw error;
-  console.log('Document updated successfully from inv_id ' + req.params.inv_id + ' to ' + req.body.inv_id);
-  client.close();
-  console.log("Closed DB connection");
-});
-}else{
-  res.status(404).json({ message: 'Invalid inventory_id' });
-}
-  client.close();
-});
+app.delete('/api/inventory/:id', (req, res) => {
+	console.log("Delete with API");
+	  if (req.params.inv_id) {
+			  let inventory = {};
+			  inventory['id'] = req.params.id;
+			  const client = new MongoClient(mongourl);
+			  client.connect((err) => {
+					assert.equal(null, err);
+					const db = client.db(dbName);
+					db.collection('Inventory').deleteMany(inventory,(err, results) =>{
+									assert.equal(err,null);
+							  client.close();
+							  res.status(200).json(results).end();
+					});
+				});
+		  }else {
+		  res.status(500).json({"error": "missing id"});       
+	  }
+  });    		
+			  
 
 
-// Delete API
-app.delete('/api/inventory/:inv_id', (req, res) => {
-  console.log("Delete with API");
-	if (req.params.inv_id) {
-    		let inventory = {};
-    		inventory['inv_id'] = req.params.inv_id;
-    		const client = new MongoClient(mongourl);
-    		client.connect((err) => {
-      			assert.equal(null, err);
-      			const db = client.db(dbName);
-      			db.collection('Inventory').deleteMany(inventory,(err, results) =>{
-      			                assert.equal(err,null);
-                			client.close();
-                			res.status(200).json(results).end();
-      			});
-      		});
-    	}else {
-        res.status(500).json({"error": "missing Inv_id"});       
-    }
-});    		
-    		
-    		
-  
-//Create the server with port 8099
+
+
 app.listen(8099);
